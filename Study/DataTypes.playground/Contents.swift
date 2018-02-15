e9ac565e0e82bfc1da760f2a7d1d71f358d79128
KimSuyoung <rumex13@gmail.com>
2018-02-16 01:52:00 +0900

    스위프트 공부

---
+++ Study/DataTypes.playground/Contents.swift
@@ -0,0 +1,27 @@
+
+var message: String = "Hello~ 123 😘"
+
+var isLogin: Bool = false
+isLogin = true
+
+var day: Int = 27
+UInt8.min
+UInt8.max
+
+Int8.min
+Int8.max
+
+Int16.min
+Int16.max
+
+Int32.min
+Int32.max
+
+Int64.min
+Int64.max
+
+
+var weight: Double = 65.51231098120931209381209381209381
+var height: Float = 175.5123801301982
+
+
---
+++ Study/Variable.playground/Contents.swift
@@ -0,0 +1,27 @@
+
+var radius = 3.123
+let pi = 3.1415926535
+radius * radius * pi
+
+radius = 5.456
+radius * radius * pi
+
+radius = 8.97
+radius * radius * pi
+
+func circleArea(r: Double) -> Double {
+    return r * r * 3.1415926535
+}
+
+circleArea(r: 3.123)
+circleArea(r: 5.456)
+circleArea(r: 8.97)
+
+class Circle {
+    var radius = 3.123
+    let pi = 3.14159265365
+    
+    func circleArea(r: Double) -> Double {
+        return r * r * 3.14159265365
+    }
+}
