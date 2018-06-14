237644c130eab0769b543dea3430f715cfc2c26c
skim <rumex13@naver.com>
2018-06-14 23:09:30 +0900

    webivew 및 mapview 구현

--- Seoul-Weather/Seoul-Weather/ChoiceViewController.swift
+++ Seoul-Weather/Seoul-Weather/ChoiceViewController.swift
@@ -12,17 +12,9 @@ class ChoiceViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        guard let destination: MapViewController = segue.destination as! MapViewController else { return }
-        
-        switch segue.identifier {
-        case .some("atype"):
-            destination.choiceType = MapViewController.MapType.Atype
-        case .some("btype"):
-            destination.choiceType = MapViewController.MapType.Btype
-        case .some("dtype"):
-            destination.choiceType = MapViewController.MapType.Dtype
-        default:
-            fatalError("지도타입을 확인해주세요")
+        if let nextViewController = segue.destination as? MapViewController {
+            nextViewController.loadMapView(identifier: segue.identifier)
         }
+        
     }
 }
